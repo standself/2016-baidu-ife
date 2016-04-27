556aba5117548f0618a6a5b087108c195ed2f18b
standself <799533994@qq.com>
2016-04-27 20:12:13 +0800

    task26.html:完成了state函数，实现了改变状态，火箭运行变化。

--- javascript/task26_js.js
+++ javascript/task26_js.js
@@ -1,5 +1,5 @@
 $(function(){
-function NewRocket(id) {
+function Rocket(id) {
 	this.id = id;
 	this.createDiv = function () {
 		var $rocket = $("<div/>"),
@@ -14,7 +14,6 @@ function NewRocket(id) {
 		$interface.append($rocket);
 		return $rocket;
 	};
-
 	var operate_timer;
 	var energy_timer;
 	var deg = 0;//运行的位置存储，旋转的角度。
@@ -36,7 +35,6 @@ function NewRocket(id) {
 			case "start":
 				operate_timer = setInterval(function(){
 					deg = deg + 10*speed;
-					console.log(deg);
 					$div.css({
 						"transform": "rotate("+deg+"deg)",
 						"transform-origin": "140px 40px",
@@ -66,28 +64,76 @@ function NewRocket(id) {
 			},200);
 		}
 	};
-
 }
 
 //状态变化
-function state(data) {
-	this.data = data;
+/*data = {
+ *	id: "1",
+ *	command: "start"
+ *}
+ */
+function State(data) {
+	this.id = data.id;
+	this.command = data.command;
+	this.info_command = $(".info .info_command");
+	this.info_state = $(".info .info_state");
+	this.rocket = $("#"+this.id);
+	this.time = new Date();
+	this.time_str = this.time.getHours() + ":" + this.time.getMinutes() + ":" + this.time.getSeconds(); 
 	this.send = function() {
 		//往命令列表里面写命令;
+		if ( !data  ) return;
+		this.info_command.append($("<p>"+"rocket #"+this.id+": "+this.command+"</br>&nbsp;&nbsp;time:"+this.time_str+"</p>"));
 	};
 	this.recieved = function() {
-		//当对应id的火箭收到消息后，往火箭状态里面写状态，格式为：rocket #id ：state。
-	}
-	this.lost = function() {
-		//就取随机数，在某个范围的就表示成丢包；不传递。
+		//当对应id的火箭收到消息后，火箭根据消息运行。往火箭状态里面写状态，格式为：rocket #id ：state。如果丢包，就什么也不干。
+		if ( !data || !lost() ) return this.info_state.append($("<p>command lost!  time:"+this.time_str+"</p>"));
+		if ( this.rocket.length == 0 ) rockets[this.id].createDiv();
+		this.info_state.append($("<p>"+"Recieved!&nbsp;rocket #"+this.id+": "+this.command+"</br>&nbsp;&nbsp;time:"+this.time_str+"</p>"));
+		rockets[this.id].command = this.command;
+		rockets[this.id].operate();
+	};
+	function lost() {
+		//就取随机数，在某个范围（0.3以内）	的就表示成丢包false；不传递。
+		var randomNum = Math.random();
+		return ( randomNum >  0.3);
 	};
 }
 
 //控制台
 function controller() {
-	//
+	var $formbtn = $("button");
+	$formbtn.on("click",function(e){
+		var event = document.event || e;
+		var target = event.target || event.srcElement;
+		var parentClass = target.parentNode.className;
+		var command = target.innerText;
+		console.log(command);
+		data.id = parentClass[parentClass.length-1];
+		data.command = command;  
+		state = new State(data);
+		state.recieved();
+	});
 }
-var div1 = new NewRocket("1");
-div1.createDiv();
-div1.operate();
+var data = {
+	id: "2",
+	command: "start"
+};
+/*rockets = {
+ *	"1": new Rocket("1"),
+ *  "2": new Rocket("2"),
+ *}
+ */
+var rockets = {
+
+    "2": new Rocket("2"),
+};
+/*rockets["2"].createDiv();
+rockets["2"].operate();
+rockets["2"].command = "start";//state要改这里的状态。
+rockets["2"].operate();*/
+controller();
+var state = new State(data);
+state.recieved();
+
 });
